93771b502005f4df1e17c8fb3da6eaefae40363a
vungvan123 <vungvan321@gmail.com>
2024-02-22 15:29:33 +0700

    update

---
+++ ProductAPI/ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly List<Product> products = new List<Product>()
+        {
+            new Product() { Id = 1, Name = "Bánh mì" },
+            new Product() { Id = 2, Name = "Kẹo lạc" }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(products);
+        }
+    }
+}
---
+++ ProductAPI/ProductAPI/Product.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductAPI
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
