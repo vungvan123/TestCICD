ef7c19e29b9c34be3e6dc746170959554467868d
vungvan123 <vungvan321@gmail.com>
2024-02-29 13:27:48 +0700

    update

--- ProductAPI/ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/ProductAPI/Controllers/ProductController.cs
@@ -15,7 +15,7 @@ namespace ProductAPI.Controllers
         {
             new Product() { Id = 1, Name = "Bánh mì" },
             new Product() { Id = 2, Name = "Kẹo lạc" },
-            new Product() { Id = 3, Name = "Cánh gà chiên mắm" },
+            //new Product() { Id = 3, Name = "Cánh gà chiên mắm" },
             //new Product() { Id = 4, Name = "Tôm chiên xù" },
             //new Product() { Id = 5, Name = "Đùi gà sốt me" },
             //new Product() { Id = 6, Name = "XXXX" },
